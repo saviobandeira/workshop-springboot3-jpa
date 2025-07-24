95956f478e16fb07d0c94568f6b4b5f82d274144
Sávio Bandeira <savio-bandeira@hotmail.com>
2025-07-23 23:37:09 -0300

    Project created

---
+++ src/test/java/com/educandoweb/course_spring/CourseSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.course_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
